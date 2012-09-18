e32e2fb322a6ce89fd5b400c7ae71e54bef04ad7
Alexander Spedding <alex_slipknot@hotmail.co.uk>
2012-09-18 21:12:56 +0100

    Parser progress

--- src/VariableType.cs
+++ src/VariableType.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace esper_compiler_v3.src
+{
+    enum VariableType
+    {
+        Integer,
+        Boolean,
+        String
+    }
+}
