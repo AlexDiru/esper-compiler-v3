1fd93b59622c9928b83525c6a37744128cfe324f
Alexander Spedding <alex_slipknot@hotmail.co.uk>
2012-09-21 21:03:26 +0100

    Basic shell for code generator created

---
+++ src/Generator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace esper_compiler_v3.src
+{
+    /// <summary>
+    /// Currently generates a basic shell of code
+    /// </summary>
+    public class Generator
+    {
+        public Root RootNode;
+
+        public void Generate()
+        {
+
+        }
+    }
+}
